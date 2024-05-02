20ff5fb82602736af5cfb70ba6a8aea11819cf72
Chis Williams <willchris917@gmail.com>
2024-05-02 15:13:49 +0530

    remove backend

---
+++ src/Login.js
@@ -0,0 +1,32 @@
+import {memo, useEffect} from 'react';
+import { useHistory, useLocation } from "react-router-dom";
+import { Button } from '@material-ui/core';
+import { auth } from './firebase';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import './Login.css'
+
+function Login(props) {
+
+    const history = useHistory();
+    const location = useLocation();
+
+    const signIn = async (token) => {
+       auth.signInWithCustomToken(token);
+    }
+
+    useEffect(async() => {
+        await signIn(props.data.token)
+    }, []);
+
+    return (
+        <div className="login">
+            <div className="login__container">
+                <div className="login__text">
+                    <h1>Please wait...</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default memo(Login)
---
+++ src/firebase.js
@@ -0,0 +1,37 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/database"
+import "firebase/messaging";
+import "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};/*use your own configuration*/
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+/*you can enable persistence to allow the user to see previous data when he's offline but it will make your queries very slow which leads
+to bad user experience so I suggest you implement your own offline support by caching the data and retrieving them when the user is offline*/
+//firebaseApp.firestore().enablePersistence();
+const db = firebaseApp.firestore();
+const db2 = firebaseApp.database();
+const auth = firebaseApp.auth();
+
+const createTimestamp = firebase.firestore.FieldValue.serverTimestamp;
+const createTimestamp2 = firebase.database.ServerValue.TIMESTAMP;
+const messaging = "serviceWorker" in navigator && "PushManager" in window ?  firebase.messaging() : null;
+const fieldIncrement = firebase.firestore.FieldValue.increment;
+const arrayUnion = firebase.firestore.FieldValue.arrayUnion;
+const storage = firebase.storage().ref("images");
+const audioStorage = firebase.storage().ref("audios");
+
+//db.disableNetwork();
+
+export { auth , createTimestamp, messaging, fieldIncrement, arrayUnion, storage, audioStorage, db2, createTimestamp2};
+export default db;
